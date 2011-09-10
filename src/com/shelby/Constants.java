c28e4e514434a3c1b04562e6436afc0d2b8ab509
flyingnome <joseph.lallouz@gmail.com>
2011-09-09 21:19:04 -0400

    first

---
+++ src/com/shelby/Constants.java
@@ -0,0 +1,5 @@
+package com.shelby;
+
+public class Constants {
+
+}
---
+++ src/com/shelby/ui/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.shelby.ui;
+
+import com.shelby.R;
+import com.shelby.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
